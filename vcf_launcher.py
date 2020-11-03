c595c6389d4f271c12378697bade7fd99a452535
Shunji Kiuchi <lunaearth445@gmail.com>
2020-11-03 16:17:54 +0900

    divide vcf

--- vcf_launcher.py
+++ vcf_launcher.py
@@ -32,20 +32,52 @@ class VCF_OT_VCFLancher(bpy.types.Operator):
         digit_func = myutil.getRoundDigit
 
         mmaps_size = round(scene.vcf_size, digit_func(scene.vcf_size))
-        display_size = round(scene.vcf1_size, digit_func(scene.vcf1_size))
+        #display_size = round(scene.vcf1_size, digit_func(scene.vcf1_size))
         spacing = round(scene.vcf_slit_spacing, digit_func(scene.vcf_slit_spacing)) 
         view_angle = round(scene.view_angle, 2)
         mbta = round(scene.mbta, 2)
-        #ior = round(scene.glass_ior, 2)
-        #mmaps.createDetailedMMAPs(size=size, spacing=spacing, detailing=detailing, height_scale=height_scale, ior=ior)
-        vcf.createVCF(size=mmaps_size, spacing=spacing, view_angle = view_angle,max_beam_transmission_angle = mbta, overwrite=False)
-        vcf.createVCF(size=display_size, spacing=spacing, view_angle = view_angle,max_beam_transmission_angle = mbta, overwrite=False)
+        vcf.createVCF(name='MMAPs' , size=mmaps_size, spacing=spacing, view_angle = view_angle,max_beam_transmission_angle = mbta)
         
     @classmethod 
     def is_exist(cls, context):
         scene = context.scene
         vcf_parent_name = scene.vcf_parent_name
         return bpy.data.objects.get(vcf_parent_name) is not None
+
+    # This function is called when "Launch" button is pressed.
+    def invoke(self, context, event):
+        if context.area.type == "VIEW_3D":
+            if self.is_exist(context):
+                print('VCF is already existed. Please be sure to clear VCF before creating it.')
+            else:
+                self.__launch(context)
+            return {'FINISHED'}
+        else:
+            return {'CANCELLED'}
+
+class VCF_OT_VCFDISLancher(bpy.types.Operator):
+    bl_idname = "object.launch_vcfdis"
+    bl_label = "VCFDIS Launch"
+    bl_description = "Launch the VCFDIS in scene."
+
+    @classmethod
+    def __launch(cls, context):
+        scene = context.scene
+        print("Start creating the VCF...")
+
+        digit_func = myutil.getRoundDigit
+
+        disp_size = round(scene.vcf1_size, digit_func(scene.vcf1_size))
+        spacing = round(scene.vcf_slit_spacing, digit_func(scene.vcf_slit_spacing)) 
+        view_angle = round(scene.view_angle, 2)
+        mbta = round(scene.mbta, 2)
+        vcf.createVCF(name = 'Display',size=disp_size, spacing=spacing, view_angle = view_angle,max_beam_transmission_angle = mbta)
+        
+    @classmethod 
+    def is_exist(cls, context):
+        scene = context.scene
+        vcfdis_parent_name = scene.vcfdis_parent_name
+        return bpy.data.objects.get(vcfdis_parent_name) is not None
 
     # This function is called when "Launch" button is pressed.
     def invoke(self, context, event):
@@ -68,13 +100,37 @@ class VCF_OT_VCFClearer(bpy.types.Operator):
         scene = context.scene
 
         vcf_louver_name = scene.vcf_louver_name
-        vcf_glass_name = scene.vcf_glass_name 
         vcf_parent_name = scene.vcf_parent_name
-        vcf.clearVCF(louver_name=vcf_louver_name, glass_name=vcf_glass_name, parent_name=vcf_parent_name)
+        vcf.clearVCF(louver_name=vcf_louver_name, glass_name='Grass', parent_name=vcf_parent_name)
 
     # This function is called when "Clear" button is pressed.
     def invoke(self, context, event):
         op_cls = VCF_OT_VCFLancher
+        if context.area.type == "VIEW_3D":
+            if op_cls.is_exist(context):
+                self.__clear(context)
+            else:
+                print("VCF is not existed.")
+            return {'FINISHED'}
+        else:
+            return {'CANCELLED'}
+        
+class VCF_OT_VCFDISClearer(bpy.types.Operator):
+    bl_idname = "object.clear_vcfdis"
+    bl_label = "VCFDIS Clear"
+    bl_description = "Clear the VCFDIS in scene."
+
+    @classmethod
+    def __clear(cls, context):
+        scene = context.scene
+
+        vcfdis_louver_name = scene.vcfdis_louver_name
+        vcfdis_parent_name = scene.vcfdis_parent_name
+        vcf.clearVCF(louver_name=vcfdis_louver_name, glass_name='Grass', parent_name=vcfdis_parent_name)
+
+    # This function is called when "Clear" button is pressed.
+    def invoke(self, context, event):
+        op_cls = VCF_OT_VCFDISLancher
         if context.area.type == "VIEW_3D":
             if op_cls.is_exist(context):
                 self.__clear(context)
@@ -94,32 +150,50 @@ class VCF_PT_VCFManager(bpy.types.Panel):
     def draw(self, context):
         scene = context.scene 
         launch_op_cls = VCF_OT_VCFLancher
+        launch_dis_op_cls = VCF_OT_VCFDISLancher
         clear_op_cls = VCF_OT_VCFClearer
+        clear_dis_op_cls = VCF_OT_VCFDISClearer
 
         layout = self.layout
         layout.use_property_split = True 
         layout.use_property_decorate = False 
-
-        # Create properties of MMAPs.  
-        layout.label(text="VCF parameters")
+        
+        # Create Display
+        layout.label(text="VCF of MMAPs parameters")
         col = layout.column()
         col.prop(scene, "vcf_size", text="MMAPs size")
+        col.prop(scene, "vcf_slit_spacing", text="Slit spacing")
+        col.prop(scene, "view_angle", text="View angle")
+        col.prop(scene, "mbta", text="Mbta")
+        row = layout.row()
+        row.operator(launch_op_cls.bl_idname, text='Launch', icon='PLAY')
+
+
+        # Create properties of MMAPs.  
+        layout.label(text="VCF of Display parameters")
+        col = layout.column()
         col.prop(scene, "vcf1_size", text="Display size")
         col.prop(scene, "vcf_slit_spacing", text="Slit spacing")
         col.prop(scene, "view_angle", text="View angle")
         col.prop(scene, "mbta", text="Mbta")
-        #col.prop(scene, "glass_ior", text="IOR (glass)")
         row = layout.row()
-        row.operator(launch_op_cls.bl_idname, text='Launch', icon='PLAY')
+        row.operator(launch_dis_op_cls.bl_idname, text='Launch', icon='PLAY')
 
         # Create object names to delete.
         layout.label(text="Object names of VCF")
         col = layout.column()
         col.prop(scene, "vcf_louver_name", text="Louver name")
-        col.prop(scene, "vcf_glass_name", text="Glass name")
         col.prop(scene, "vcf_parent_name", text="VCF name")
         row = layout.row()
         row.operator(clear_op_cls.bl_idname, text='Clear', icon='REMOVE')
+
+        # Create object names to delete.
+        layout.label(text="Object names of VCFDIS")
+        col = layout.column()
+        col.prop(scene, "vcfdis_louver_name", text="Louver name")
+        col.prop(scene, "vcfdis_parent_name", text="VCFDIS name")
+        row = layout.row()
+        row.operator(clear_dis_op_cls.bl_idname, text='Clear', icon='REMOVE')
 
 def init_props():
     scene = bpy.types.Scene 
@@ -153,26 +227,23 @@ def init_props():
         min = 0,
         default=25
     )
-    
-    '''
-    scene.glass_ior = FloatProperty(
-        name="Index of reflaction of glass.",
-        min = 0.1,
-        default=1.52
-    )
-    '''
 
     scene.vcf_louver_name = StringProperty(
         name="The name of louver object.",
         default="Louver"
     )
-    scene.vcf_glass_name = StringProperty(
-        name="The name of glass object.",
-        default = "Glass"
-    )
     scene.vcf_parent_name = StringProperty(
         name="The name of VCF",
-        default="VCF"
+        default="MMAPsVCF"
+    )
+    
+    scene.vcfdis_louver_name = StringProperty(
+        name="The name of louver object.",
+        default="Louver"
+    )
+    scene.vcfdis_parent_name = StringProperty(
+        name="The name of VCFDIS",
+        default="DisplayVCF"
     )
 
 def clear_props():
@@ -182,14 +253,16 @@ def clear_props():
     del scene.vcf_slit_spacing
     del scene.view_angle
     del scene.mbta
-    #del scene.glass_ior
     del scene.vcf_louver_name
-    del scene.vcf_glass_name
     del scene.vcf_parent_name
+    del scene.vcfdis_louver_name
+    del scene.vcfdis_parent_name
 
 classes = [
     VCF_OT_VCFLancher,
+    VCF_OT_VCFDISLancher,
     VCF_OT_VCFClearer,
+    VCF_OT_VCFDISClearer,
     VCF_PT_VCFManager,
 ]               
 
